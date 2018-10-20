f849f6d5bf4ee79fd7b6a6c5167d35e9ea1f8040
dambusm <willschaller@gmail.com>
2018-10-20 12:15:14 +0200

    Add placeholders for substitution

--- app/src/main/java/mgks/os/webview/SmartWebView.java
+++ app/src/main/java/mgks/os/webview/SmartWebView.java
@@ -12,23 +12,23 @@ class SmartWebView {
 
 	//Permission variables
 	static boolean ASWP_JSCRIPT     = true;     //enable JavaScript for webview
-	static boolean ASWP_FUPLOAD     = true;     //upload file from webview
-	static boolean ASWP_CAMUPLOAD   = true;     //enable upload from camera for photos
+	static boolean ASWP_FUPLOAD     = ${uploads};     //upload file from webview
+	static boolean ASWP_CAMUPLOAD   = ${camera};     //enable upload from camera for photos
  	static boolean ASWP_MULFILE     = false;    //upload multiple files in webview; not stable yet
-	static boolean ASWP_LOCATION    = true;     //track GPS locations
-	static boolean ASWP_RATINGS     = true;     //show ratings dialog; auto configured, edit method get_rating() for customizations
-	static boolean ASWP_PBAR        = true;     //show progress bar in app
-	static boolean ASWP_ZOOM        = false;    //zoom control for webpages view
+	static boolean ASWP_LOCATION    = ${gps};     //track GPS locations
+	static boolean ASWP_RATINGS     = ${ratings};     //show ratings dialog; auto configured, edit method get_rating() for customizations
+	static boolean ASWP_PBAR        = ${progressBar};     //show progress bar in app
+	static boolean ASWP_ZOOM        = ${zoom};    //zoom control for webpages view
 	static boolean ASWP_SFORM       = false;    //save form cache and auto-fill information
 	static boolean ASWP_OFFLINE     = false;    //whether the loading webpages are offline or online
-	static boolean ASWP_EXTURL      = true;     //open external url with default browser instead of app webview
+	static boolean ASWP_EXTURL      = ${externalUrls};     //open external url with default browser instead of app webview
 
 	//Configuration variables
-	static String ASWV_URL          = "http://mgks.infeeds.com/"; //complete URL of your website or webpage
+	static String ASWV_URL          = "${url}"; //complete URL of your website or webpage
 	static String ASWV_F_TYPE       = "*/*";  //to upload any file type using "*/*"; check file type references for more
 
 	//Rating system variables
-	static int ASWR_DAYS            = 3;        //after how many days of usage would you like to show the dialoge
+	static int ASWR_DAYS            = ${ratingDays};        //after how many days of usage would you like to show the dialoge
 	static int ASWR_TIMES           = 10;       //overall request launch times being ignored
 	static int ASWR_INTERVAL        = 2;        //reminding users to rate after days interval
 }
